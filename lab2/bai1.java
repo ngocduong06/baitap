76b28a0183b21b782875ce694a6ead6c7da36a59
ngocduong06 <tranngocduong0602@gmail.com>
2024-01-24 12:35:52 +0700

    lab2

---
+++ lab2/bai1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class bai1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a,b;
+        System.out.println("nhap vao so a:");
+        a= sc.nextDouble();
+        System.out.println("nhap vao so b:");
+        b=sc.nextDouble();
+        System.out.println("tong cua 2 so a va b la:" +(a+b));
+        System.out.println(" hieu cua 2 so a vav b la:" +(a-b) +"\t"+(b-a));
+        System.out.println("tich cua 2 so la:" +(a*b));
+        System.out.println(" thuong cua 2 so a va b la:" +(a/b) + "\t"+(b/a));
+        System.out.println("chia lay phan du cua 2 so a va b:" +(a%b) + "\t"+(b%a));
+    }
+    
+}
---
+++ lab2/bai2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class bai2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a;
+        System.out.println("nhap gia tri cua so a:");
+        a=sc.nextDouble();
+        if(a%2==0){
+            System.out.println("a la so chan:");
+        }
+        else{
+            System.out.println("a la so le");
+        }
+          
+    }
+}
