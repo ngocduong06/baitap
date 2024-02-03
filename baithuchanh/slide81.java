17a9882263e571cbf689dfd6018f1e34ac4d104f
ngocduong06 <tranngocduong0602@gmail.com>
2024-02-04 03:12:00 +0700

    slide81chuong2

---
+++ baithuchanh/slide81.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class slide81 {
+    public static void main(String[]args) {
+        Scanner sc = new Scanner(System.in);
+        int n,m,i,j,max=0;
+        System.out.println("nhap so hang:");
+        m=sc.nextInt();
+        System.out.println("nhap so cot:");
+        n=sc.nextInt();
+        int [][]a=new int[m][n];
+         for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                System.out.print("Nhap phan tu thu  [" + i + ", " + j + "]: ");
+                a[i][j]=sc.nextInt();
+            }
+        }
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                System.out.print("ma tran vua nhap la:"+a[i][j] +"\t");
+            }
+            System.out.println("\n");
+        }
+     
+        for(i=0;i<m;i++){
+            for(j=0;j<n;j++){
+                if(max<a[i][j])
+                    max=a[i][j];
+            }
+        }
+        System.out.println("gia tri lon nhat trong ma tran la:"+max);
+        }
+    }
