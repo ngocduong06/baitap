aa501309f336f1087f262387eb0a2c1131656ea2
ngocduong06 <tranngocduong0602@gmail.com>
2024-02-04 03:53:00 +0700

    bai7 chuong2

---
+++ lab2/bai7.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class bai7 {
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("20 so nguyen dau tien:");
+        for(int i=1;i<21;i++){
+           System.out.println("\t"+i);
+        }
+    }
+}
