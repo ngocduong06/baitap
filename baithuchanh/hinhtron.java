ed9281cd8fd6f1f04e5cf322ea909a5046f511a5
ngocduong06 <tranngocduong0602@gmail.com>
2024-03-15 01:12:30 +0700

    slide16chuong3

---
+++ baithuchanh/hinhtron.java
@@ -0,0 +1,9 @@
+ import java.util.Scanner;
+ public class hinhtron {
+    public static void main(String[] args) {
+        lophinhtron a= new lophinhtron();
+        a.nhap();
+        a.tinh();
+        a.hienthi();
+    } 
+}
---
+++ baithuchanh/lophinhtron.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+public class lophinhtron {
+     final float Pi =3.14f;
+    float r,dt,cv;
+   public  void nhap(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap vao ban kinh cua hinh tron:");
+        r=sc.nextFloat();
+    }
+    public void tinh(){
+        cv=2*Pi*r;
+        dt=Pi*r*r;
+    }
+    public void hienthi(){
+        System.out.print("chu vi hinh tron voi ban kinh"+r+"la:"+cv);
+        System.out.print("/tdien tich hinh tron voi ban kinh"+r+"la:"+dt);
+    }
+}
